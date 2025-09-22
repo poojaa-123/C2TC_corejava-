29cfa14e18f85a153ec8600f1bfb7a70fd8829ef
Pooja sree <poojasree2301@gmail.com>
2025-09-22 17:38:18 +0530

    commit

---
+++ Corejava_programs/src/DAY1/FIRSTPROGRAM.java
@@ -0,0 +1,11 @@
+package DAY1;
+
+public class FIRSTPROGRAM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to java.....");
+
+	}
+
+}
