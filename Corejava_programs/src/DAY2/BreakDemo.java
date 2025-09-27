453644fa757922d7ca3232b58c979e0d06052b4a
Pooja sree <poojasree2301@gmail.com>
2025-09-27 11:17:17 +0530

    commit

---
+++ Corejava_programs/src/DAY2/BreakDemo.java
@@ -0,0 +1,18 @@
+package DAY2;
+
+public class BreakDemo {
+	
+    // This is the correct main method for the JVM to execute.
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    	for (int i = 5; i < 10; i++) {
+    		
+    	  if (i == 5)
+    	        break; // This immediately exits the loop on the very first iteration (i=5).
+    	  
+    	   System.out.println(i);
+    	}
+    }
+}  
+    	
+      
