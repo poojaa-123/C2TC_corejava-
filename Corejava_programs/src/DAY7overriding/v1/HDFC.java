2cc9dcbf18149f3f1aad672b8b0034d354cc0632
Pooja sree <poojasree2301@gmail.com>
2025-10-03 18:04:47 +0530

    commit

---
+++ Corejava_programs/src/DAY7overriding/v1/HDFC.java
@@ -0,0 +1,8 @@
+package DAY7overriding.v1;
+public class HDFC extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.8f;
+	}
+
+}
---
+++ Corejava_programs/src/DAY7overriding/v1/ICICI.java
@@ -0,0 +1,8 @@
+package DAY7overriding.v1;
+public class ICICI extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.9f;
+	}
+
+}
