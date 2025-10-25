063db7f3d0a4a191798e34ced65e3b58dcf38d92
Pooja sree <poojasree2301@gmail.com>
2025-10-25 11:39:32 +0530

    commit

---
+++ Corejava_programs/src/DAY9/StringSubclass.java
@@ -0,0 +1,18 @@
+package DAY9;
+
+import javax.print.DocFlavor.STRING;
+
+public class StringSubclass extends STRING {
+
+	public StringSubclass(String mimeType) {
+		super(mimeType);
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final long serialVersionUID = -4284660459006757630L;
+
+	public static void main(String[] args) {
+		
+
+	}
+}
